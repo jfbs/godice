9e0a994e915eb6c246c184c0bfb47e48cbb7913c
jfbs <phrewn@gmail.com>
2016-09-05 05:43:07 -0400

    print hands in order. TODO: parse tabs

--- tryna.go
+++ tryna.go
@@ -5,9 +5,6 @@ import ("fmt"; "math/rand"; "time"; "strconv"; "os"; "bufio"; "strings";)
 func main() {
 	sixrolls := []string{}
 	sixrolls =  getsix()
-	/* for v:= range sixrolls {
-		fmt.Println(sixrolls[v])  // print each hand
-	} */
 
 	file, err := os.Open("diceware.txt")
 	if err != nil {
@@ -17,32 +14,19 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
-	// delimiter := '\t'
-	// vartemp := scanner.Text()
-	// rightofdel := vartemp.Join(strings.Split(vartemp, delimiter)[1:], delimiter)
 
-	varg := make([]string, 6)
+	varg := make([]string, 6) // store matches into slice
 	for scanner.Scan() {
 		for d := 0; d < 6; d++ {
 			if strings.Contains(scanner.Text(), sixrolls[d]) {
 				varg[d] = scanner.Text()
-				//varg = append(varg, scanner.Text())
-				fmt.Printf("Roll %d is: %v\n", d, scanner.Text())
 			}
 		}
 	}
 
-	fmt.Println("first is: ", sixrolls[0]) // check first roll
-	fmt.Println("second is: ", sixrolls[1])
-	fmt.Println("third is: ", sixrolls[2])
-
-	fmt.Println("first varg is: ", varg[0])
-	fmt.Println("second varg is: ", varg[1])
-	fmt.Println("third varg is: ", varg[2])
   for e := range varg {
-		fmt.Printf("Roll %d is: %v\n", e, varg[e])
+		fmt.Printf("Roll %d is: %v\n", e+1, varg[e])
 	}
-	//fmt.Println(varg)
 
 	if err := scanner.Err(); err != nil {
 		checkError(err)
