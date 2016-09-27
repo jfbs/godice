cfe5632d5290c7768da585364ab5727591801543
jfbs <phrewn@gmail.com>
2016-09-27 01:54:11 -0400

    fix source tabs

--- tryna.go
+++ tryna.go
@@ -5,69 +5,69 @@ import ("fmt"; "math/rand"; "time"; "strconv"; "os"; "bufio"; "strings";)
 const number_of_phrases int = 8 // change for desired phrase count
 
 func main() {
-	rolled_hand := []string{}
-	rolled_hand =  getrolls()
+  rolled_hand := []string{}
+  rolled_hand =  getrolls()
 
-	file, err := os.Open("diceware.txt")
-	if err != nil {
-		checkError(err)
-	}
-	defer file.Close()
+  file, err := os.Open("diceware.txt")
+  if err != nil {
+    checkError(err)
+  }
+  defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
+  scanner := bufio.NewScanner(file)
+  scanner.Split(bufio.ScanLines)
 
-	phrase_id := make([]string, number_of_phrases) // store matches into slice
-	for scanner.Scan() {
-		for d := range rolled_hand {
-			if strings.Contains(scanner.Text(), rolled_hand[d]) {
-				phrase_id[d] = scanner.Text() // match
-			}
-		}
-	}
+  phrase_id := make([]string, number_of_phrases) // store matches into slice
+  for scanner.Scan() {
+    for d := range rolled_hand {
+      if strings.Contains(scanner.Text(), rolled_hand[d]) {
+        phrase_id[d] = scanner.Text() // match
+      }
+    }
+  }
 
-	fmt.Printf("Passphrase: ")
+  fmt.Printf("Passphrase: ")
   for e := range phrase_id {
-		input := string(phrase_id[e])
-		trimmed := strings.Replace(input, "\t"," ",-1)
-		parts := strings.Split(trimmed, " ")
-		fmt.Printf(parts[1] + " ")
-	}
-	fmt.Printf("\n")
+    input := string(phrase_id[e])
+    trimmed := strings.Replace(input, "\t"," ",-1)
+    parts := strings.Split(trimmed, " ")
+    fmt.Printf(parts[1] + " ")
+  }
+  fmt.Printf("\n")
 
-	if err := scanner.Err(); err != nil {
-		checkError(err)
-	}
+  if err := scanner.Err(); err != nil {
+    checkError(err)
+  }
 }
 
 func randInt(min int, max int) int {
-	rand.Seed(time.Now().UTC().UnixNano())
-	return min + rand.Intn(max - min)
+  rand.Seed(time.Now().UTC().UnixNano())
+  return min + rand.Intn(max - min)
 }
 
 func rolldice() string {
-	var fivedice string
+  var fivedice string
 
-	for j := 0; j < 5; j++ { // roll a six-sided die five times
-		fivedice += strconv.Itoa(randInt(1,7))
-	}
+  for j := 0; j < 5; j++ { // roll a six-sided die five times
+    fivedice += strconv.Itoa(randInt(1,7))
+  }
 
-	return fivedice
+  return fivedice
 }
 
 func getrolls() []string {
-	rollstr := []string{}
+  rollstr := []string{}
 
-	for k := 0; k < number_of_phrases; k++ { // roll five dice for each phrase
-		rollstr = append(rollstr, rolldice())
-	}
+  for k := 0; k < number_of_phrases; k++ { // roll five dice for each phrase
+    rollstr = append(rollstr, rolldice())
+  }
 
-	return rollstr
+  return rollstr
 }
 
 func checkError(err error) {
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(0)
-	}
+  if err != nil {
+    fmt.Println(err.Error())
+    os.Exit(0)
+  }
 }
